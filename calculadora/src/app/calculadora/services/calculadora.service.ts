401ff43fd8e9d4e0fcd75cd3d08dae058b531937
Rafael Baena Neto <5616063-rafaelbaena@users.noreply.gitlab.com>
2020-05-03 17:29:15 -0300

    Aula 33 e 34 Implementando e Documentado o serviço de Calculadora

--- calculadora/src/app/calculadora/services/calculadora.service.ts
+++ calculadora/src/app/calculadora/services/calculadora.service.ts
@@ -5,5 +5,45 @@ import { Injectable } from '@angular/core';
 })
 export class CalculadoraService {
 
+  /**
+   * Define as constantes utilizadas para identificar as operações de cálculo
+  */
+  static readonly SOMA: string = '+';
+  static readonly SUBTRACAO: string = '-';
+  static readonly DIVISAO: string = '/';
+  static readonly MULTIPLICACAO: string = '*';
+
   constructor() { }
+
+  /**
+   * Método que calcula uma operação matemática dado dois números.
+   * Suporta as operações sima, subtração, divisão, e multiplicação
+   * 
+   * @param num1 number
+   * @param num2 number
+   * @param operação string Operação a ser executada
+   */
+  calcular(num1:number, num2: number, operacao: string): number{
+    let resutado: number;
+
+    switch(operacao) {
+      case CalculadoraService.SOMA:
+        resutado = num1 + num2;
+      break;
+      case CalculadoraService.SUBTRACAO:
+        resutado = num1 - num2;
+      break;
+      case CalculadoraService.MULTIPLICACAO:
+        resutado = num1 * num2;
+      break;
+      case CalculadoraService.DIVISAO:
+        resutado = num1 / num2;
+      break;
+      default:
+        resutado = 0;
+    }
+    
+    return resutado;
+  }
+
 }
